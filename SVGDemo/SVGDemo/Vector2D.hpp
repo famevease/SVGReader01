efc0c3bb18db48827515602a0c7201f008be78bb
Hireophant <tntdanh2506@gmail.com>
2025-06-25 20:58:46 +0700

    vector

---
+++ SVGDemo/SVGDemo/Vector2D.hpp
@@ -0,0 +1,24 @@
+﻿#ifndef vector2D_hpp_
+#define vector2D_hpp
+/*
+* Class biểu diễn vector với 2 trục x, y.
+* Dùng để biểu diễn các đại lượng có 2 chiều: size, point, ...
+* Tempalte parameter T là 1 loại tọa độ. Hô trợ các phép toán số học (+, -, /, *), comparison (==, !=), vd int hoặc float
+*/
+template<typename T>
+class Vector2D {
+public:
+	Vector2D(); //Default cóntructor
+	Vector2D(T X, T Y); //Thay tọa độ cụ thể
+	template<typename U>
+	explicit Vector2D(const Vector2D<U>& vector); //Chuyển từ kiểu từ vector2D<int> sang vector2D<float>
+	T x;
+	T y;
+};
+
+template<typename T>
+Vector2D<T> operator-(const Vector2D<T>& right); //chuyển ngược vector
+template<typename T>
+Vector2D<T> operator*(const Vector2D<T>& left, T right)
+
+#endif
