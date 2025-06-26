bb334a6870712eb6f0bbf043a809b58ce3ed486d
famevease <trangiahien2006@gmail.com>
2025-06-26 20:49:52 +0700

    Ellipse

--- SVGDemo/SVGDemo/SVGElement.cpp
+++ SVGDemo/SVGDemo/SVGElement.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "SVGElement.h"
 
-#include <iostream>
-
 SVGElement::SVGElement() : fill(ColorShape::Black), stroke(ColorShape::Transparent), stroke_width(0), gradient(NULL) {}
 
 SVGElement::SVGElement(const ColorShape& fill, const ColorShape& stroke, float stroke_width) : fill(fill), stroke(stroke), stroke_width(stroke_width), gradient(NULL) {}
@@ -54,25 +52,25 @@ Vector2Df SVGElement::getMaxBound() const {
     return Vector2Df(); 
 }
 
-void SVGElement::printData() const {
-    std::cout << "Shape: " << getShape() << std::endl;
-    std::cout << "Fill: " << getFillColor() << std::endl;
-    std::cout << "Stroke: " << getOutlineColor() << std::endl;
-    std::cout << "Stroke width: " << getOutlineThickness() << std::endl;
-    std::cout << "Position: " << getPosition().x << " " << getPosition().y
-        << std::endl;
-    std::cout << "Transforms: ";
-    for (auto transform : transforms) {
-        std::cout << transform << " ";
-    }
-    std::cout << std::endl;
-    if (gradient != NULL)
-        std::cout << "Gradient: " << gradient->getClass() << " "
-        << gradient->getPoints().first.x << " "
-        << gradient->getPoints().first.y << " "
-        << gradient->getPoints().second.x << " "
-        << gradient->getPoints().second.y << std::endl;
-}
+//void SVGElement::printData() const {
+//    std::cout << "Shape: " << getShape() << std::endl;
+//    std::cout << "Fill: " << getFillColor() << std::endl;
+//    std::cout << "Stroke: " << getOutlineColor() << std::endl;
+//    std::cout << "Stroke width: " << getOutlineThickness() << std::endl;
+//    std::cout << "Position: " << getPosition().x << " " << getPosition().y
+//        << std::endl;
+//    std::cout << "Transforms: ";
+//    for (auto transform : transforms) {
+//        std::cout << transform << " ";
+//    }
+//    std::cout << std::endl;
+//    if (gradient != NULL)
+//        std::cout << "Gradient: " << gradient->getClass() << " "
+//        << gradient->getPoints().first.x << " "
+//        << gradient->getPoints().first.y << " "
+//        << gradient->getPoints().second.x << " "
+//        << gradient->getPoints().second.y << std::endl;
+//}
 
 void SVGElement::setTransforms(const std::vector< std::string >& transforms) {
     this->transforms = transforms;
